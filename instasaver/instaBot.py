037fb12ce8a710bbb5097a58c8269c30c699117c
Unanoc <danya4og@mail.ru>
2018-05-17 01:49:05 +0300

    added parser and connecting bot, which is able to download 1 jpg and reply 1 jpg

--- instasaver/instaBot.py
+++ instasaver/instaBot.py
@@ -2,6 +2,8 @@ import telebot
 import config
 import datetime
 
+from parser import saveByURL
+
 bot = telebot.TeleBot(config.token_telegram)
 
 def logger(msg):
@@ -10,20 +12,28 @@ def logger(msg):
         log = config.logForm.format(logData, msg.chat.first_name, msg.chat.last_name, msg.chat.username,msg.text)
     else:
         log = msg
-
     try:
         print(log)
         config.logFile(log)
     except Exception as ex:
         config.logFile(msg=ex)
 
-
-
 @bot.message_handler(commands=["start"])
 def greeting(message):
     logger(msg=message)
     bot.send_message(message.chat.id, text="Hello, {}!".format(message.chat.first_name))
 
+@bot.message_handler(content_types=["text"])
+def urls(message):
+    logger(msg=message)
+    if saveByURL(message.text):
+        bot.send_message(message.chat.id, text="SUCCESS!")
+        photo = open("../cat3.jpg", 'rb')
+        bot.send_photo(message.chat.id, photo)
+    else:
+        bot.send_message(message.chat.id, text="FAILURE!")
+
+    
 
 
 
--- instasaver/parser.py
+++ instasaver/parser.py
@@ -2,15 +2,50 @@
 import argparse
 import json
 import re
-import webbrowser
-
 import requests
 from bs4 import BeautifulSoup
 
-
-pageHTML = requests.get("https://www.instagram.com/p/BixTQqbgPT_/?taken-by=gal_gadot")
-soup = BeautifulSoup(pageHTML.content, 'lxml')
+import config
 
 
+GRAPH_SIDECAR_REG_ARGS = '"edge_sidecar_to_children":', '}}}]},"gatekeepers":'
+GRAPH_SINGLE_MEDIA_REG_ARGS = '"shortcode_media":', 'racking_token'
+FROM_TO_REGEX_PATTERN = r'(?<=({})).*(?=({}))'
 
-print(soup)
+def getMediaFromNode(node):
+    return node['display_url'] if not node['is_video'] else node['video_url']
+
+
+def graphSidecarSearch(jsonInDict):
+    for edge in jsonInDict['edges']:
+        yield getMediaFromNode(edge['node'])
+
+
+def getContent(soup):
+    script = soup.find('body').find('script', type='text/javascript').text
+    if 'GraphSidecar' in script:
+        foundedJSON = re.search(FROM_TO_REGEX_PATTERN.format(*GRAPH_SIDECAR_REG_ARGS), script)
+        parsedNodes = json.loads(foundedJSON.group(0))
+        for mediaURL in graphSidecarSearch(parsedNodes):
+            yield mediaURL
+    else:
+        foundedJSON = re.search(FROM_TO_REGEX_PATTERN.format(*GRAPH_SINGLE_MEDIA_REG_ARGS), script)
+        parsedNode = json.loads(foundedJSON.group(0).replace(',"t', '}'))  # oh shit shame...
+        yield getMediaFromNode(parsedNode)
+
+
+def saveByURL(instagrmURL):
+    try:
+        print(instagrmURL)
+        pageHTML = requests.get(instagrmURL)
+        soup = BeautifulSoup(pageHTML.content, 'lxml')
+
+        for url in getContent(soup):
+            r = requests.get(url)
+            with open('../cat3.jpg', 'wb') as f:  
+                f.write(r.content)
+
+        return True
+    except Exception as ex:
+        config.logFile(msg=ex)
+        return False
