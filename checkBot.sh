aa820e5a6d5f2f985814126eeea83bfc84ff8d88
Unanoc <danya4og@mail.ru>
2018-05-18 11:37:39 +0300

    added bash script for supporting bot

---
+++ checkBot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# returns count of files in dir
+#ls -1 | wc -l
+
+# screen proccesses are here
+#/var/run/screen/S-root
+
+# if count of screen proccesses is 0, then run bot
+if [ $(ls -1 /var/run/screen/S-root | wc -l) -eq 0 ]
+then
+    source /home/instasaver/venv/bin/activate && screen -d -m python3 /home/instasaver/instasaver/instaBot.py
+fi
+
+# chmod +x checkBot.sh
+# -**- in crontab write the following: -**-
+#  * * * * * /home/checkBot.sh
